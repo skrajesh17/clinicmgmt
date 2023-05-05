50635c302c3980b4dea7d1cf93689197277f01e3
skrajesh17 <rajesh@sysalgo.com>
2023-05-05 21:34:46 +0530

    Basic Layouts

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,42 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'page_title' => " Home",
+
+        ];
+        
+        return view("homeView", $data);
+        
     }
+    public function about(){
+        $data = [
+            'page_title' => " About",
+        ];
+
+        return view("aboutView", $data);
+    }
+
+    public function patients(){
+        $data = [
+            'page_title' => " Patients List",
+        ];
+
+        return view("patientsView", $data);
+    }
+
+    public function reports(){
+        $data = [
+            'page_title' => "Reports List",
+        ];
+        return view("reportsView", $data);
+    }
+
+    public function doctors(){
+        $data = [
+            'page_title' => "Doctors List",
+        ];
+        return view("doctorsView", $data);
+    }
+
 }
